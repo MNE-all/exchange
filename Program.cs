00a6dbe114e9e3f30f426f5c1f4882282e9557b5
MNE4 <monashovnikolaye@gmail.com>
2022-11-05 16:33:24 +0300

    add logger(Seq)

--- Program.cs
+++ Program.cs
@@ -1,31 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Serilog;
+using Serilog.Core;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
 namespace bank                                                                // Баловался с пространством имён
 {
     class Trade
     {
         public static void DollarToRu()
         {
-            Console.Write("Введите ваш баланс в долларах($): ");
-            var userInput = Console.ReadLine();
+            
             decimal RuCourse = 63.7m, comission = 0.37m, balance = 0.0m;
-            while(!decimal.TryParse(userInput, out balance))                     // Ждём корректного ввода пользователя
-            {
-                Console.Write("Введите ваш баланс в долларах($): ");
-                userInput = Console.ReadLine();
-            }
-            var ruBalance = balance > 500 ? balance * RuCourse / 100 * (100 - comission): balance * RuCourse - 8; 
+            var logger = new LoggerConfiguration().MinimumLevel.Information()
+             .Enrich.WithProperty("Курс: ", RuCourse)
+             .WriteTo.Seq("http://localhost:5341", apiKey: "RAu7ePctKZSChRb2hqdS")
+             .CreateLogger();
+            var userInput = "null";
 
-                Console.WriteLine($"Ваш баланс в рублях по курсу 63,7 с учётом комиссии: {ruBalance: ###,###.##} руб.");
+            do                    
+            {
+                if (userInput != "null")
+                {
+                    logger.Error("Обмен не прошёл!");
+
+                }
+                Console.Write("Введите ваш баланс в долларах($): ");
+
+                logger.Information("Пользователь вводит информацию");
+
+                userInput = Console.ReadLine();
+            } while (!decimal.TryParse(userInput, out balance));            // Ждём корректного ввода пользователя
+            logger.Information($"Ввод прошел успешно. Пользователь ввел {balance} Долларов");
+
+            logger.Information(balance > 500 ? 
+                $"Комиссия(%): {comission}" : 
+                $"Комиссия(руб.): {8}");
+
+            var ruBalance = balance > 500 ? 
+                balance * RuCourse / 100 * (100 - comission): 
+                balance * RuCourse - 8; 
+
+            Console.WriteLine("Ваш баланс в рублях по курсу 63,7 с учётом комиссии: {0:C2}", ruBalance);
+
+            logger.Information("Обмен прошел успешно. Пользователь получил {0:C2} (руб.)", ruBalance);
+
+
         }
     }
-    class Program                                       // Тут я просто баловался с классами, по сути можно всё сделать через один Main()
+    class Program                                       
     {
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Trade.DollarToRu();
-            Console.ReadLine();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            do
+            {
+                Trade.DollarToRu();
+                Console.WriteLine("Для выхода - нажмите Ecs\n" +
+                    "Нажмите любую другую клавишу для выхода");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
